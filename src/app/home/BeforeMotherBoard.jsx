1efb5caf4f8f93dd0125cc707b96ef21a727ad36
Soham <mastpanchalsoham@gmail.com>
2023-12-26 21:03:41 +0530

    fix: removed fullscreen prop form BeforeMotherBoard

--- src/app/home/BeforeMotherBoard.jsx
+++ src/app/home/BeforeMotherBoard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const BeforeMotherBoard = () => {
   return (
-    <Html fullscreen >
+    <Html >
     <div className='h-screen w-screen bg-[#a3d7ff]'>
         <div className='flex items-center justify-center w-full h-full text-[#3b5060] text-2xl font-bold'>
             Get Ready to Explore the MLSC Motherboard!
