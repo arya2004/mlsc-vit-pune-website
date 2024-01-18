eb969a494760d7b18ca6214634dd2088d4a49407
Soham <mastpanchalsoham@gmail.com>
2024-01-19 00:02:32 +0530

    feat: Added glitch animation to the html text in the motherboard scene

--- src/app/home/html-cards/About.card.jsx
+++ src/app/home/html-cards/About.card.jsx
@@ -1,51 +1,62 @@
-import { Html, Text } from "@react-three/drei";
+import { Html, Text, Text3D } from "@react-three/drei";
 import React from "react";
+import './cards_style.scss'
 
-// const About = () => {
-//   return (
-//     <Html>
-//       <div className="align-center flex flex-col items-center justify-evenly w-[400px] h-[350px] p-3 bg-transparent border-[2px] border-[#0078d4] rounded-[8px]  text-center text-[#0078d4]">
-//         <div className="font-bold">
-//           Welcome to Microsoft Learn Student Club VIT, Pune:
-//         </div>
-//         <div className="font-semibold">
-//           Join us on a journey of exploration and innovation at Microsoft Learn
-//           Student Club VIT, Pune! As passionate learners and tech enthusiasts,
-//           we strive to create a dynamic community that fosters collaboration and
-//           skill development. Whether you're a seasoned developer or just
-//           starting your tech journey, there's a place for you here.
-//         </div>
-//       </div>
-//     </Html>
-//   );
-// };
+import cn from '../../utils/cn'
 
 const AboutMesh = () => {
-  return (
-    <>
-      <group position={[2.6, 1.1, -3]} rotation={[0, -Math.PI / 6, 0]}>
-        <Text
-          fontSize={0.11}
-          position={[-.2, 0.5, 0]}
-          scale={[1, 1, 1]}
-          // color="#0078d4"
-        >
-          Welcome to Microsoft Learn Student Club VIT, Pune:{"\n"}
-          <meshBasicMaterial color="#20151f" />
-        </Text>
+  let timer = true;
+  console.log(`TIMER1: ${timer}`);
 
-        <Text  position={[-.2, 0, 0]} fontSize={0.1} scale={[1, 1, 1]} color="#20151f">
-          Join us on a journey of exploration and {"\n"} innovation at Microsoft
-          Learn Student Club VIT, Pune! {"\n"} As passionate learners and tech
-          enthusiasts, {"\n"}
-          we strive to create a dynamic community that {"\n"} fosters
-          collaboration and skill development. {"\n"} Whether you're a seasoned
-          developer or just {"\n"}
-          starting your tech journey, there's a {"\n"} place for you here.
-        </Text>
-      </group>
-    </>
+  setTimeout(() => {
+    timer = false;
+    console.log(`TIMER2: ${timer}`);
+  }, 4000);
+
+  return (
+    <Html>
+      <div className="container align-center flex flex-col items-center justify-evenly w-[500px] h-[350px] p-3 bg-transparent rounded-[8px] text-2xl text-center text-[#94b3fdbd]">
+        <div className={cn(!timer ?"font-bold":"font-bold glitch")} data-text="Welcome to Microsoft Learn Student Club VIT, Pune">
+          Welcome to Microsoft Learn Student Club VIT, Pune:
+        </div>
+        <div className="font-semibold">
+          Join us on a journey of exploration and innovation at Microsoft Learn
+          Student Club VIT, Pune! As passionate learners and tech enthusiasts,
+          we strive to create a dynamic community that fosters collaboration and
+          skill development. Whether you're a seasoned developer or just
+          starting your tech journey, there's a place for you here.
+        </div>
+      </div>
+    </Html>
   );
 };
+
+// const AboutMesh = () => {
+//   return (
+//     <>
+//       <group position={[2.6, 1.1, -3]} rotation={[0, -Math.PI / 6, 0]}>
+//         <Text3D
+          
+//           position={[-.2, 0.5, 0]}
+//           scale={[1, 1, 1]}
+//           // color="#0078d4"
+//         >
+//           Welcome to Microsoft Learn Student Club VIT, Pune:{"\n"}
+//           <meshBasicMaterial color="#20151f" />
+//         </Text3D>
+
+//         <Text3D  position={[-.2, 0, 0]} scale={[1, 1, 1]} >
+//           Join us on a journey of exploration and {"\n"} innovation at Microsoft
+//           Learn Student Club VIT, Pune! {"\n"} As passionate learners and tech
+//           enthusiasts, {"\n"}
+//           we strive to create a dynamic community that {"\n"} fosters
+//           collaboration and skill development. {"\n"} Whether you're a seasoned
+//           developer or just {"\n"}
+//           starting your tech journey, there's a {"\n"} place for you here.
+//         </Text3D>
+//       </group>
+//     </>
+//   );
+// };
 
 export default AboutMesh;
