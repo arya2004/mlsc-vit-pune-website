23406d862d1fa6b5bda5a3f7ac1cdbd92fa293d6
Soham <mastpanchalsoham@gmail.com>
2024-01-21 16:28:39 +0530

    Created a rough glitch effect on the text

--- src/app/home/html-cards/About.card.jsx
+++ src/app/home/html-cards/About.card.jsx
@@ -1,10 +1,11 @@
 import { Html, Text, Text3D } from "@react-three/drei";
 import React from "react";
-import './cards_style.scss'
+import "./cards_style.scss";
 
-import cn from '../../utils/cn'
+import cn from "../../utils/cn";
 
 const AboutMesh = () => {
+  
   let timer = true;
   console.log(`TIMER1: ${timer}`);
 
@@ -13,18 +14,29 @@ const AboutMesh = () => {
     console.log(`TIMER2: ${timer}`);
   }, 4000);
 
+  const text =
+    "Join us on a journey of exploration and innovation at Microsoft Learn Student Club VIT, Pune! As passionate learners and tech enthusiasts, we strive to create a dynamic community that fosters collaboration and skill development. Whether you're a seasoned developer or just starting your tech journey, there's a place for you here.";
+  const textArray = text.split(" ");
+
   return (
     <Html>
-      <div className="container align-center flex flex-col items-center justify-evenly w-[500px] h-[350px] p-3 bg-transparent rounded-[8px] text-2xl text-center text-[#94b3fdbd]">
-        <div className={cn(!timer ?"font-bold":"font-bold glitch")} data-text="Welcome to Microsoft Learn Student Club VIT, Pune">
+      <div className="container align-center flex flex-col items-center justify-evenly w-[550px] h-[350px] p-3 bg-transparent rounded-[8px] text-2xl text-center text-[#00040cbd] uppercase  stable-shadows">
+        <div
+          className={cn(!timer ? "font-bold" : "font-bold glitch")}
+          data-text="Welcome to Microsoft Learn Student Club VIT, Pune"
+        >
           Welcome to Microsoft Learn Student Club VIT, Pune:
         </div>
         <div className="font-semibold">
-          Join us on a journey of exploration and innovation at Microsoft Learn
-          Student Club VIT, Pune! As passionate learners and tech enthusiasts,
-          we strive to create a dynamic community that fosters collaboration and
-          skill development. Whether you're a seasoned developer or just
-          starting your tech journey, there's a place for you here.
+          {textArray.map((word, index) => (
+            <span
+              key={index}
+              className={"glitch"}
+              data-text={word+" "}
+            >
+              {word}{" "}
+            </span>
+          ))}
         </div>
       </div>
     </Html>
@@ -36,7 +48,7 @@ const AboutMesh = () => {
 //     <>
 //       <group position={[2.6, 1.1, -3]} rotation={[0, -Math.PI / 6, 0]}>
 //         <Text3D
-          
+
 //           position={[-.2, 0.5, 0]}
 //           scale={[1, 1, 1]}
 //           // color="#0078d4"
