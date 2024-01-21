2b501bcc893d2391e84791df48e4a3c55842857b
arya2004 <arya20j4@gmail.com>
2024-01-22 00:12:12 +0530

    feat: next-auth with prisma and protected routes with cookies and jtw

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Page() {
     <main className="flex min-h-screen flex-col items-center justify-between p-0">
       {/* <Home /> */}
       <MotherBoardCity />
-  
+
     </main>
   )
 }
