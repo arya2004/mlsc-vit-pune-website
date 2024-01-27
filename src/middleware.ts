5b3fec45166e708f310ff2701ca643cb2fdb533c
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2024-01-27 17:55:50 +0530

    fix: protected paths
    Signed-off-by: SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,8 @@
- export { default } from 'next-auth/middleware'
+export { default } from 'next-auth/middleware'
 
 export const config = {
-  matcher: ['/protected',
+  matcher: ['/protected/:path*', '/protected',
     // '/api/:path*',
-]
+  ]
 }
 // Add your route in above config.matcher array to protect it
