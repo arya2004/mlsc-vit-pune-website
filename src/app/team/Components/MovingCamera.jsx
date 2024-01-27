1fbc635d8c0780f4c135083863b984efcb18d877
Soham <mastpanchalsoham@gmail.com>
2024-01-27 16:48:56 +0530

    feat: Added a pointer lock component and rapier physics library for First person controls.

---
+++ src/app/team/Components/MovingCamera.jsx
@@ -0,0 +1,60 @@
+import React, { useRef } from "react";
+import { useKeyboardControls } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { CapsuleCollider, RigidBody, useRapier } from "@react-three/rapier";
+
+import * as THREE from "three";
+import RAPIER from "@dimforge/rapier3d-compat";
+
+const frontVector = new THREE.Vector3();
+const sideVector = new THREE.Vector3();
+const direction = new THREE.Vector3();
+const rotation = new THREE.Vector3();
+
+function MovingCamera() {
+  const controls = useRef();
+  const [, get] = useKeyboardControls();
+
+  const rapier = useRapier();
+
+  useFrame((state, delta) => {
+    state.camera.position.set(controls.current.translation());
+
+    const { forward, backward, left, right } = get();
+    const velocity = controls.current.linvel();
+    console.log(controls);
+    // console.log(velocity);
+
+    frontVector.set(0, 0, backward - forward);
+    sideVector.set(left - right, 0, 0);
+    direction
+      .subVectors(frontVector, sideVector)
+      .normalize()
+      .multiplyScalar(5)
+      .applyEuler(state.camera.rotation);
+
+    controls.current.setLinvel([direction.x, velocity.y, direction.z]);
+
+    const ray = rapier.world.castRay(
+      new RAPIER.Ray(controls.current.translation(), { x: 0, y: -1, z: 0 })
+    );
+    const ground = ray && ray.collider && Math.abs(ray.toi) <= 1.75;
+  });
+
+  return (
+    // <PerspectiveCamera position={[1, 4, 2]} ref={controls} />
+    <>
+      <RigidBody
+        type='dynamic'
+        ref={controls}
+        mass={1}
+        position={[0, 25, 0]}
+        enabledRotations={[false, false, false]}
+      >
+        <CapsuleCollider args={[0.75, 0.5]} />
+      </RigidBody>
+    </>
+  );
+}
+
+export default MovingCamera;
--- src/app/team/page.jsx
+++ src/app/team/page.jsx
@@ -7,9 +7,14 @@ import {
   Box,
   Octahedron,
   Environment,
+  KeyboardControls,
+  KeyboardControlsEntry,
+  FirstPersonControls,
+  PerspectiveCamera,
+  PointerLockControls,
 } from "@react-three/drei";
 import { Canvas, extend } from "@react-three/fiber";
-import React, { Suspense, useRef } from "react";
+import React, { Suspense, useRef, useMemo } from "react";
 
 import { Background } from "./Components/Background.jsx";
 import Effects from "./Components/Effects.jsx";
@@ -22,9 +27,12 @@ import * as THREE from "three";
 import Fillers from "./Components/Fillers.jsx";
 import Portal from "./Components/Portal.jsx";
 import MainPlatform from "./Components/MainPlatform.jsx";
+import MovingCamera from "./Components/MovingCamera.jsx";
+import { Physics, RigidBody, CapsuleCollider } from "@react-three/rapier";
 
 function page() {
   const sun = useRef();
+  
 
   const backgroundColors = useRef({
     // colorB: "#61b0ed",
@@ -32,22 +40,42 @@ function page() {
     colorA: "#001e4e",
   });
 
+
   return (
     <div className="w-screen h-screen bg-transparent">
-      <Canvas style={{ height: "100%", width: "100%" }} shadows='soft'>
-        <OrbitControls />
+      <KeyboardControls
+        map={[
+          { name: "forward", keys: ["ArrowUp", "w", "W"] },
+          { name: "backward", keys: ["ArrowDown", "s", "S"] },
+          { name: "left", keys: ["ArrowLeft", "a", "A"] },
+          { name: "right", keys: ["ArrowRight", "d", "D"] },
+          { name: "jump", keys: ["Space"] },
+        ]}
+      >
+        <Canvas style={{ height: "100%", width: "100%" }} shadows="soft" camera={{position: [0, 5, 15]}}>
+          {/* <OrbitControls /> */}
 
-        {/* <Suspense >
-          <Portal />
-        </Suspense> */}
-        <Suspense fallback='spinner' >
-          <MainPlatform position={[0, -20, 0]} />
-        </Suspense>
+          {/* <Suspense >
+            <Portal />
+          </Suspense> */}
+          <Suspense fallback="spinner">
+            <Physics gravity={[0, -10, 0]} >
+              <PointerLockControls  />
+              <MovingCamera />
+            
+                  
+         
+              <RigidBody type="fixed" colliders='trimesh' position={[0, -20, 0]}>
+                <MainPlatform />
+              </RigidBody>
+            </Physics>
+          </Suspense>
 
-        <Background backgroundColors={backgroundColors} />
-        <Fillers />
-        <Effects />
-      </Canvas>
+          <Background backgroundColors={backgroundColors} />
+          <Fillers />
+          <Effects />
+        </Canvas>
+      </KeyboardControls>
       <Loader />
     </div>
   );
