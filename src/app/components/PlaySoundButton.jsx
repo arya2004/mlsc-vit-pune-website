acd12c43472d24d8a7f80418e5f6d945f5e4b28e
Soham <mastpanchalsoham@gmail.com>
2024-02-03 11:37:41 +0530

    feat: Added AIFill icons to the play sound button

--- src/app/components/PlaySoundButton.jsx
+++ src/app/components/PlaySoundButton.jsx
@@ -1,8 +1,8 @@
-import { GiSoundOn, GiSoundOff, AiFillSound, AiOutlineSound } from "react-icons/ai";
+import { AiFillSound, AiOutlineSound } from "react-icons/ai";
 
 function PlaySoundButton({setPlayBGM, playBGM}) {
   return(
-    <button onClick={() => setPlayBGM(!playBGM)} className="absolute flex items-center justify-center top-8 left-12 w-10 h-10 rounded-[8px] border-1 border-[blue] bg-slate-800 text-[] ">
+    <button onClick={() => setPlayBGM(!playBGM)} className="absolute flex items-center justify-center bottom-8 right-12 w-10 h-10 rounded-[8px] border-1 border-[blue] bg-slate-800 cursor-pointer ">
         {/* {playBGM ? <GiSoundOn className="w-full h-full"/> : <GiSoundOff className="w-full h-full"/>} */}
         {playBGM ? <AiFillSound className="w-7 h-7" />: <AiOutlineSound className="w-7 h-7" />}
     </button>
