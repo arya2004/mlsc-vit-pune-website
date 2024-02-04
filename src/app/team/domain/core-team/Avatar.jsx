875801117893b0fedc56f032b696ecfe0699135f
Soham <mastpanchalsoham@gmail.com>
2024-02-05 00:34:34 +0530

    feat: Added avatar in the core-team page, also platform and added animation

--- src/app/team/domain/core-team/Avatar.jsx
+++ src/app/team/domain/core-team/Avatar.jsx
@@ -1,7 +1,7 @@
-import React, { useRef, useMemo } from 'react'
+import React, { useRef, useMemo, useEffect } from 'react'
 import { useGraph } from '@react-three/fiber'
 import { SkeletonUtils } from 'three-stdlib'
-import { useGLTF, useAnimations } from '@react-three/drei'
+import { useGLTF, useAnimations, useFBX } from '@react-three/drei'
 
 
 export function Avatar(props) {
@@ -11,9 +11,29 @@ export function Avatar(props) {
     const clone = useMemo(() => SkeletonUtils.clone(scene), [scene]);
     const { nodes } = useGraph(clone);
 
+    const { animations: bBoy_hip_hop } = useFBX('/animations/Bboy Hip Hop Move.fbx');
+    const { animations: gangnum_style } = useFBX('/animations/Gangnam Style.fbx');
+    const { animations: hip_hop} = useFBX('/animations/Hip Hop Dancing.fbx');
+    const { animations: house_dancing} = useFBX('/animations/House Dancing.fbx');
+    const { animations: swing_dancing} = useFBX('/animations/Swing Dancing.fbx');
+
+    bBoy_hip_hop[0].name = "Bboy Hip Hop Move";
+    gangnum_style[0].name = "Gangnum Style";
+    hip_hop[0].name = "Hip Hop Dance";
+    house_dancing[0].name = "House Dancing";
+    swing_dancing[0].name = "Swing Dancing";
+    
+    const {actions} = useAnimations([bBoy_hip_hop[0], gangnum_style[0], hip_hop[0], house_dancing[0], swing_dancing[0]], group);
+
+    useEffect(() => {
+        actions['Gangnum Style'].play();
+    }, [actions]);
+    
+    
+
     // const { actions } = useAnimations(animations, group)
     return (
-      <group ref={group} {...props} dispose={null}>
+      <group castShadow ref={group} {...props} dispose={null}>
         <primitive object={clone}  />
       </group>
     );
--- src/app/team/domain/core-team/page.jsx
+++ src/app/team/domain/core-team/page.jsx
@@ -1,26 +1,37 @@
-'use client'
-import { Loader, OrbitControls, Stage } from "@react-three/drei";
+"use client";
+
+import { Loader, OrbitControls, Stage, AccumulativeShadows, RandomizedLight, PerspectiveCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 
-import {Avatar} from './Avatar.jsx';
-
+import { Avatar } from "./Avatar.jsx";
+import { AvatarPlatform } from "../../Components/AvatarPlatform.jsx"
 
 function Page() {
-    return (
-        <div>
-            <Canvas style={{'width':'100w', 'height':'100vh'}} camera={{ position: [0, 10, 30], near: 0.05, far: 1000, fov: 50 }} >
-                <OrbitControls />
-                <color attach="background" args={['#000']} />
+  return (
+    <>
+      <Canvas
+        style={{ width: "100w", height: "100vh" }}
+        // camera={{ position: [0, 1.8, 6], target: [0, 3, 0], near: 0.05, far: 50, fov: 50 }}
+        shadows='soft'
+      >
+        {/* <OrbitControls /> */}
+        <PerspectiveCamera makeDefault position={[0, 1.5, 6]} lookAt={[0, .5, 0]} near={0.05} far={50} fov={50} />
+        <color attach="background" args={["#010114"]} />
 
-                <ambientLight intensity={3} />
-                <axesHelper args={[5]} />
-                <Stage preset='soft' scale={[1, 1, 1]} position={[0, 1, 0]} adjustCamera={0} shadows >
-                    <Avatar position={[0, 2.5, 0]} rotation={[0, 0, 0]} />
-                </Stage>
-            </Canvas>
-            <Loader />
-        </div>
-    );
+        {/* <ambientLight intensity={3} /> */}
+        <directionalLight position={[0, 2, 5]} color='#88b6f5' intensity={5} castShadow />
+        <axesHelper args={[5]} />
+
+        <AvatarPlatform scale={[.002, .002, .002]} position={[0, 0, 0]} />
+        <AccumulativeShadows receiveShadow color="#010114"  temporal frames={100} scale={50} colorBlend={2} toneMapped={true} alphaTest={0.75} opacity={2}>
+          <RandomizedLight intensity={Math.PI} radius={4} amount={8} ambient={0.5} bias={0.001} position={[5, -1, -10]} />
+        </AccumulativeShadows> 
+
+        <Avatar position={[0, .4, 0]} rotation={[0, 0, 0]} />
+      </Canvas>
+      <Loader />
+      </>
+  );
 }
 
 export default Page;
