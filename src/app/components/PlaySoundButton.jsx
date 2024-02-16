58d017b1ac9de14e26912c9535575d413a3afe77
Soham <mastpanchalsoham@gmail.com>
2024-02-16 18:51:31 +0530

    feat: Installed sass and react-icons packages

--- src/app/components/PlaySoundButton.jsx
+++ src/app/components/PlaySoundButton.jsx
@@ -1,10 +1,10 @@
-import { GiSoundOn, GiSoundOff, AiFillSound, AiOutlineSound } from "react-icons/ai";
+// import { GiSoundOn, GiSoundOff, AiFillSound, AiOutlineSound } from "react-icons/ai";
 
 function PlaySoundButton({setPlayBGM, playBGM}) {
   return(
     <button onClick={() => setPlayBGM(!playBGM)} className="absolute flex items-center justify-center top-8 left-12 w-10 h-10 rounded-[8px] border-1 border-[blue] bg-slate-800 text-[] ">
         {/* {playBGM ? <GiSoundOn className="w-full h-full"/> : <GiSoundOff className="w-full h-full"/>} */}
-        {playBGM ? <AiFillSound className="w-7 h-7" />: <AiOutlineSound className="w-7 h-7" />}
+        {/* {playBGM ? <AiFillSound className="w-7 h-7" />: <AiOutlineSound className="w-7 h-7" />} */}
     </button>
   )
 }
