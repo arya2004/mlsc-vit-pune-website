ad43deb5e5f1b10aa65e42631ce34f1861ef3e6e
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2024-02-17 21:20:28 +0530

    feat: re-validating the cache
    Signed-off-by: SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
-import Home from './home/Home'
+import axios from 'axios'
 import MotherBoardCity from './home/MotherBoardCity'
 
 export default function Page() {
+  const cacheServerUrl = process.env.NEXT_APP_CACHE_VALIDATOR_SERVER_URL
+  axios.get(cacheServerUrl + "/hit/all")
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-0">
       {/* <Home /> */}
