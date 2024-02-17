a7528cd216b50bc379aa8156259b2d0187290dda
Soham <mastpanchalsoham@gmail.com>
2024-02-17 23:51:48 +0530

    tryouts: Just trying out different changes to implement first person controls.

--- src/app/team/Components/MovingCamera.jsx
+++ src/app/team/Components/MovingCamera.jsx
@@ -2,6 +2,7 @@ import React, { useRef } from "react";
 import { useKeyboardControls } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { CapsuleCollider, RigidBody, useRapier } from "@react-three/rapier";
+import { Suspense } from "react";
 
 import * as THREE from "three";
 import RAPIER from "@dimforge/rapier3d-compat";
@@ -9,52 +10,73 @@ import RAPIER from "@dimforge/rapier3d-compat";
 const frontVector = new THREE.Vector3();
 const sideVector = new THREE.Vector3();
 const direction = new THREE.Vector3();
-const rotation = new THREE.Vector3();
+// const rotation = new THREE.Vector3();
 
 function MovingCamera() {
   const controls = useRef();
-  const [, get] = useKeyboardControls();
+  console.log(controls);
+  const [_, get] = useKeyboardControls();
 
-  const rapier = useRapier();
+  // const rapier = useRapier();
 
   useFrame((state, delta) => {
-    state.camera.position.set(controls.current.translation());
+    const conCurr = controls.current;
+    if (conCurr !== undefined) {
+      state.camera.position.set(conCurr?.translation());
 
-    const { forward, backward, left, right } = get();
-    const velocity = controls.current.linvel();
-    console.log(controls);
-    // console.log(velocity);
+      const { forward, backward, left, right } = get();
+      // console.log("FORWARD "+forward);
+      // console.log("BACKWARD "+backward);
+      // console.log("LEFT "+left);
+      // console.log("RIGHT "+right);
+      const velocity = conCurr?.linvel();
 
-    frontVector.set(0, 0, backward - forward);
-    sideVector.set(left - right, 0, 0);
-    direction
-      .subVectors(frontVector, sideVector)
-      .normalize()
-      .multiplyScalar(5)
-      .applyEuler(state.camera.rotation);
+      // console.log(controls);
+      console.log(velocity);
 
-    controls.current.setLinvel([direction.x, velocity.y, direction.z]);
+      frontVector.set(0, 0, backward - forward);
+      sideVector.set(left - right, 0, 0);
 
-    const ray = rapier.world.castRay(
-      new RAPIER.Ray(controls.current.translation(), { x: 0, y: -1, z: 0 })
-    );
-    const ground = ray && ray.collider && Math.abs(ray.toi) <= 1.75;
+      console.log(frontVector);
+
+      direction
+        .subVectors(frontVector, sideVector)
+        .normalize()
+        .multiplyScalar(5)
+        // .applyEuler(state.camera.rotation);
+
+        try {
+          // conCurr?.setLinvel([0, 5, 15]);
+        } catch (error) {
+          console.error("Error setting linear velocity:", error);
+        }
+    }
+
+    // const world = rapier.world.raw();
+    // const ray = world?.castRay(
+    //   new RAPIER.Ray(controls.current?.translation(), { x: 0, y: -1, z: 0 })
+    // );
+    // const ground = ray && ray.collider && Math.abs(ray.toi) <= 1.75;
   });
 
   return (
     // <PerspectiveCamera position={[1, 4, 2]} ref={controls} />
-    <>
-      <RigidBody
-        type='dynamic'
-        ref={controls}
-        mass={1}
-        position={[0, 25, 0]}
-        
-        enabledRotations={[false, false, false]}
-      >
-        <CapsuleCollider args={[0.75, 0.5]} />
-      </RigidBody>
-    </>
+
+    <RigidBody
+      type="dynamic"
+      ref={controls}
+      mass={1}
+      position={[0, 10, 15]}
+      restitution={0.3}
+      colliders={false}
+      enabledRotations={[false, false, false]}
+    >
+      <CapsuleCollider args={[0.75, 0.5]} />
+      {/* <mesh>
+        <boxGeometry args={[2, 2, 2]} />
+        <meshStandardMaterial color="red" />
+      </mesh> */}
+    </RigidBody>
   );
 }
 
--- src/app/team/page.jsx
+++ src/app/team/page.jsx
@@ -32,14 +32,12 @@ import { Physics, RigidBody, CapsuleCollider } from "@react-three/rapier";
 
 function page() {
   const sun = useRef();
-  
 
   const backgroundColors = useRef({
     // colorB: "#61b0ed",
     colorB: "#001e4e",
     colorA: "#001e4e",
   });
-
 
   return (
     <div className="w-screen h-screen bg-transparent">
@@ -49,31 +47,44 @@ function page() {
           { name: "backward", keys: ["ArrowDown", "s", "S"] },
           { name: "left", keys: ["ArrowLeft", "a", "A"] },
           { name: "right", keys: ["ArrowRight", "d", "D"] },
-          { name: "jump", keys: ["Space"] },
         ]}
       >
-        <Canvas style={{ height: "100%", width: "100%" }} shadows="soft" camera={{position: [0, 5, 15]}}>
+        <Canvas
+          style={{ height: "100%", width: "100%" }}
+          shadows="soft"
+          // camera={{ position: [0, 5, 30] }}
+        >
           {/* <OrbitControls /> */}
+          <ambientLight intensity={5} />
 
           {/* <Suspense >
             <Portal />
           </Suspense> */}
-          <Suspense fallback="spinner">
-            <Physics gravity={[0, -10, 0]} >
-              <PointerLockControls  />
+          <Suspense>
+            <Physics gravity={[0, -10, 0]} debug >
+              <PointerLockControls />
               <MovingCamera />
-            
-                  
-         
-              <RigidBody type="fixed" colliders='trimesh' position={[0, -20, 0]}>
+
+              {/* <RigidBody type="dynamic" mass={1} restitution={1} position={[0, 10, 15]} enabledRotations={[false, false, false]}>
+              <mesh>
+                <boxGeometry args={[2, 2, 2]} />
+                <meshStandardMaterial color="hotpink" />
+              </mesh>
+              </RigidBody> */}
+
+              <RigidBody
+                type="fixed"
+                colliders="trimesh"
+                position={[0, -20, 0]}
+              >
                 <MainPlatform />
               </RigidBody>
             </Physics>
           </Suspense>
 
           <Background backgroundColors={backgroundColors} />
-          <Fillers />
-          <Effects />
+          {/* <Fillers /> */}
+          {/* <Effects /> */}
         </Canvas>
       </KeyboardControls>
       <Loader />
