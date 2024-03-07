afc8922d5b427d45252a9da5be9ce6e81671b8ff
arya2004 <arya20j4@gmail.com>
2024-03-07 14:17:18 +0530

    feat: domain name filter

--- src/app/api/team/route.ts
+++ src/app/api/team/route.ts
@@ -1,10 +1,11 @@
 
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { kv } from '@vercel/kv';
 import prisma from '../../../../prisma/client'
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../auth/[...nextauth]/route";
+import { NextApiRequest, NextApiResponse } from "next";
 
 //Route handlers
 //https://nextjs.org/docs/app/building-your-application/routing/route-handlers
@@ -12,12 +13,30 @@ import { authOptions } from "../auth/[...nextauth]/route";
 
 
 
-export async function GET(request: Request,) {
+export async function GET(request: NextRequest, response: NextResponse) {
+  const searchParams = request.nextUrl.searchParams;
+  const domainName = searchParams.get("domain");
+  console.log(domainName);
+  
+  if(domainName != null){
 
+  }
+  
 
   try {
     const cached = await kv.get("teams");
     if (cached) {
+
+
+      if (domainName != null) {
+        const filteredTeams = (cached as Array<any>).filter((team: { domain: string }) => team.domain === domainName);
+
+        return NextResponse.json({
+          cached: filteredTeams,
+          status: 200
+        });
+      }
+
       console.log("Team Chache HIT")
       return NextResponse.json({
           cached,
@@ -27,6 +46,21 @@ export async function GET(request: Request,) {
 
     } else {
       console.log("Team Chache MISS")
+
+      if(domainName != null){
+        const data = await prisma.user.findMany({
+          where: {
+            domain: domainName
+          }
+        });
+        return NextResponse.json({
+          data,
+          status: 200
+        }
+        );
+      }
+
+
       const data = await prisma.user.findMany({});
       return NextResponse.json({
         data,
