fbfcf3044827e06159a53252a11bb0266a79ec4b
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2024-03-09 18:11:17 +0530

    fix: uploader
    Signed-off-by: SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>

--- src/app/api/photo/upload/route.ts
+++ src/app/api/photo/upload/route.ts
@@ -1,10 +1,10 @@
 import { put } from '@vercel/blob';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function POST(request: Request): Promise<NextResponse> {
+export async function POST(request: NextRequest): Promise<NextResponse> {
     const { searchParams } = new URL(request.url);
-    const filename = searchParams.get('filename');
-
+    const randomNumber = Math.floor(Math.random() * 1000000);
+    const filename = searchParams.get('filename') || `image${randomNumber}.jpg`;
     const blob = await put(filename, request.body, {
         access: 'public',
     });
