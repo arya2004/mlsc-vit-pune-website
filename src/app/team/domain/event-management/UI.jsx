cbedfcdd47f74dba7b2f5a4ab1cac920a284dd69
Soham <mastpanchalsoham@gmail.com>
2024-03-15 18:54:48 +0530

    feat: positioning Html UI's according to viewport width for responsiveness.

--- src/app/team/domain/event-management/UI.jsx
+++ src/app/team/domain/event-management/UI.jsx
@@ -28,8 +28,12 @@ export function YearDept() {
 export function SocialIcons() {
 
     const gl = useThree((state) => state.gl);
+    const viewPort = useThree((state) => state.viewport);
+    console.log("VIEWPORT:", viewPort.width);
+    const positionFactor = viewPort.width / 25;
+
     return (
-        <Html scale={[0.5, 0.5, 0.5]} position={[10,-2,-0.7]} transform portal={{ current: gl.domElement.parentNode }} >
+        <Html scale={[0.5, 0.5, 0.5]} position={[10*positionFactor,-2,-0.7]} transform portal={{ current: gl.domElement.parentNode }} >
             <div className="w-auto h-auto p-0 ">
                 <img className="" src="/images/svgs/social-icons-bg.svg" />
 
@@ -52,8 +56,12 @@ export function SocialIcons() {
 export function NameYearDept(){
 
     const gl = useThree((state) => state.gl);
+    const viewPort = useThree((state) => state.viewport);
+
+    const positionFactor = viewPort.width / 25;
+
     return(
-        <Html scale={[1, 1, 1]} position={[-8,2.5,-0.7]} occlude transform portal={{ current: gl.domElement.parentNode }} >
+        <Html scale={[1, 1, 1]} position={[-8*positionFactor,2.5,-0.7]} occlude transform portal={{ current: gl.domElement.parentNode }} >
             <div className=" w-3/5">
                 <div className="font-Wallpoet text-3xl text-white">
                     Soham Panchal{/* TODO: Name */}
