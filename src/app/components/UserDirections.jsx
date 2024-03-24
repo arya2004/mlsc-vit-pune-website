fa8825b7acaba0ea2a19e831dae60f6cd019d2a5
Soham <mastpanchalsoham@gmail.com>
2024-03-25 01:15:48 +0530

    feat: Added mouse direction in the directional overlay ui for wasd.

--- src/app/components/UserDirections.jsx
+++ src/app/components/UserDirections.jsx
@@ -50,8 +50,13 @@ export function WASDMotion() {
         <div className={cn(show?"w-screen h-screen":"opacity-0 pointer-events-none w-0 h-0 ","top-0 left-0 right-0 bottom-0 absolute flex items-center justify-center backdrop-blur-sm ease-in-out duration-500")}>
             <div onClick={()=>setShow(false)} className="bg-cover bg-center bg-[url('/images/svgs/spo-button.svg')] w-96 h-72 hover:brightness-150 flex items-center justify-center text-lg text-[#f0f0f0] font-bold cursor-pointer ease-in-out duration-100">Ok</div>
             <div className={cn(show?"w-72 h-36":"w-0 h-0 opacity-0 pointer-events-none" ,"absolute bottom-8 right-8 flex flex-row text-[#f0f0f0] bg-black ease-in-out duration-200")}>
-            <div className="w-1/2 h-full text-sm p-1 text-wrap">
+            <div className="w-1/2 h-full">
+             <div className="w-full h-1/2 text-sm p-1 text-wrap">
                 Use <u>w-a-s-d</u> or <u>arrow</u> keys to move, press <u>space</u> to jump
+             </div>
+             <div className="w-full h-1/2 text-sm p-1 text-wrap">
+                <div className="bg-cover bg-center bg-[url('/icons/mouse-icon.png')] w-6 h-6" /> use mouse to look around
+             </div>
             </div>
             <div className="w-1/2 h-full bg-center bg-cover bg-[url('/icons/wasd-icon.png')]" />
         </div>
