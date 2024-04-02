f655963bcb1f83fd2b6b09bf1eb5a1dba742defb
arya2004 <arya20j4@gmail.com>
2024-04-02 18:29:00 +0530

    feat: cloudinary upload

---
+++ src/app/admin-panel/member-images/page.tsx
@@ -0,0 +1,32 @@
+'use client';
+import { CldUploadWidget } from 'next-cloudinary';
+import { useState } from 'react'; // Import useState hook
+
+export default function MemberImagesPage() {
+  const [uploadSuccess, setUploadSuccess] = useState(false); // State for tracking upload success
+
+  const handleUploadSuccess = () => {
+    setUploadSuccess(true); // Set upload success to true
+    setTimeout(() => {
+      setUploadSuccess(false); // Reset upload success after 5 seconds
+    }, 5000);
+  };
+
+  return (
+    <div style={{color: 'green'}}>
+      {uploadSuccess && (
+        <div style={{ color: 'green', marginBottom: '10px' }}>
+          Upload successful!
+        </div>
+      )}
+      <CldUploadWidget
+        uploadPreset="hbebpk3m"
+        onSuccess={() => handleUploadSuccess()} // Call handleUploadSuccess on successful upload
+      >
+        {({ open }) => (
+          <button onClick={() => open()}>Upload an Image</button>
+        )}
+      </CldUploadWidget>
+    </div>
+  );
+}
